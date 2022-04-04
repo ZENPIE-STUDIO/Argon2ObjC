5f10a5fc6e442bac28fccc8fc4802028e467083f
zenpie.studio <zenpie.studio@gmail.com>
2022-04-04 15:45:39 +0800

    試SwiftPM - ObjC Framework

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Argon2ObjC",
-    platforms: [
-    	.macOS(.v10_14),
-    	.iOS(.v13)
-    ],
+//    platforms: [
+//    	.macOS(.v10_14),
+//    	.iOS(.v13)
+//    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "Argon2ObjC", targets: ["Argon2ObjC"]),
@@ -23,6 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
     	.target(
     		name: "Argon2ObjC",
+    		path: "."
+    		sources: [
+    			"Argon2ObjC/Argon2Hash.m",
+    			"Argon2ObjC/ZPUtils.m",
+    		],
     		dependencies: ["argon2"]
     	),
 //    	.binaryTarget(
